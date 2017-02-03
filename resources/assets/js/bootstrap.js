45fc9111db28afc45d1c8a65da9862bb3707dc24
Mehran <mehran.hadidi@gmail.com>
2017-02-03 14:58:57 +0330

    create a test build to test vuex is importing correctly in bootstrap js. (run npm dev executed)

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -20,6 +20,12 @@ require('bootstrap-sass');
 window.Vue = require('vue');
 
 /**
+ * Import Vuex for using inside vue
+ */
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
