a3def165578a2a165960191231bd350bfc977e1f
Mehran <mehran.hadidi@gmail.com>
2017-02-12 13:18:30 +0330

    create relations & some helper between user & conversation

--- app/Conversation.php
+++ app/Conversation.php
@@ -2,9 +2,95 @@
 
 namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Conversation extends Model
 {
-    //
+    protected $dates = ['last_reply'];
+
+    protected $fillable = [
+        //
+    ];
+
+    /**
+     * Owner of this conversation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Owners of this conversation
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function users()
+    {
+        return $this->belongsToMany(User::class);
+    }
+
+    /**
+     *
+     *
+     * @return mixed
+     */
+    public function usersExceptCurrentlyAuthenticatedUser()
+    {
+        return $this->user()->where('user_id', '!=', \Auth::user()->id);
+    }
+
+    /**
+     *
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function replies()
+    {
+        return $this->hasMany(Conversation::class, 'parent_id')->latestFirst();
+    }
+
+    /**
+     *
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function parent()
+    {
+        return $this->belongsTo(Conversation::class, 'parent_id');
+    }
+
+    /**
+     *
+     */
+    public function touchLastReply()
+    {
+        $this->last_reply = \Carbon\Carbon::now();
+        $this->save();
+    }
+
+    /**
+     * Check if this conversation is a reply
+     *
+     * @return bool
+     */
+    public function isReply()
+    {
+        return $this->parent_id !== null;
+    }
+
+    /**
+     * Local scope to order query results in desc format
+     *
+     * @param $query
+     * @return mixed
+     */
+    public function scopeLatestFirst($query)
+    {
+        return $query->orderBy('created_at', 'desc');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -26,4 +26,37 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     * All conversations related to the user
+     *
+     * @return mixed
+     */
+    public function conversations()
+    {
+        return $this->belongsTo(Conversation::class)->whereNull('parent_id')->orderBy('last_reply', 'desc');
+    }
+
+    /**
+     * Check if a user is in a given conversation or not
+     *
+     * @param Conversation $conversation
+     * @return mixed
+     */
+    public function isInConversation(Conversation $conversation)
+    {
+        return $this->conversations->contains($conversation);
+    }
+
+    /**
+     * Get user's avatar
+     *
+     * @param int $size
+     * @return string
+     * @link http://en.gravatar.com/support/what-is-gravatar/
+     */
+    public function avatar($size = 45)
+    {
+        return 'https://www.gravatar.com/avatar/' . md5($this->email) . '?size=' . $size . '&d=mm';
+    }
 }
